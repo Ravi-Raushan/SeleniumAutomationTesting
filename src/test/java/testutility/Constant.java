1bc741994c0da2b5a6743c624583669d948e8cbf
Ravi-Raushan <krravi015@gmail.com>
2020-06-17 22:23:32 +0530

    [Ravi Raushan]Added Constant class which have Constant term that used in this project

---
+++ src/test/java/testutility/Constant.java
@@ -0,0 +1,16 @@
+package testutility;
+
+public class Constant {
+        public static final String WEB_DRIVER_NAME = "webdriver.chrome.driver";
+        public static final String WEB_DRIVER_PATH = "C:\\Users\\Ravi Raushan\\Downloads\\chromedriver_win32\\chromedriver.exe";
+        public static final String LOGIN_URL = "http://localhost:4200/login";
+        public static final String REGISTER_URL = "http://localhost:4200/register";
+        public static final String FORGOT_PASSWORD_URL = "http://localhost:4200/forgotpassword";
+        public static final String EXCEL_FILE_PATH = "C:\\Users\\Ravi Raushan\\IdeaProjects\\FundooTestingUsingSelenium\\src\\test\\resources\\testdata\\fundooAppTestData.xlsx";
+        public static final String EXCEL_OUTPUT_FILE_PATH = "C:\\Users\\Ravi Raushan\\IdeaProjects\\FundooTestingUsingSelenium\\src\\test\\resources\\testdata\\fundooAppOutput.xlsx";
+        public static final String LOGIN_DATA_PROVIDER = "fundooAppLoginData";
+        public static final String FORGOT_PASSWORD_DATA_PROVIDER = "fundooAppForgotPasswordData";
+        public static final String REGISTRATION_DATA_PROVIDER = "fundooAppRegistrationData";
+        public static final String SCREENSHOT_FILE_PATH = "C:\\Users\\Ravi Raushan\\IdeaProjects\\FundooTestingUsingSelenium\\Screenshot\\";
+        public static final String REPORT_FILE_PATH = "C:\\Users\\Ravi Raushan\\IdeaProjects\\FundooTestingUsingSelenium\\src\\test\\testreport\\";
+}
